37c239effbc5df7207b95fda374cb53125bf4a01
Furball <33634370+Eeveelution@users.noreply.github.com>
2022-04-22 00:01:23 +0200

    Remove pprof for now as I don't need it

--- main.go
+++ main.go
@@ -2,17 +2,10 @@ package main
 
 import (
 	"Waffle/waffle"
-	"log"
-	"net/http"
-	_ "net/http/pprof"
 	"time"
 )
 
 func main() {
-	go func() {
-		log.Println(http.ListenAndServe("localhost:6060", nil))
-	}()
-
 	bancho := waffle.CreateBancho()
 	go bancho.RunBancho()
 
