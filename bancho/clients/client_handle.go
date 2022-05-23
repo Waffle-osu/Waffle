d2a51d9a4048a11613b74a2fb18f46e27c134bb9
Furball <33634370+Eeveelution@users.noreply.github.com>
2022-05-23 19:14:10 +0200

    commented a bit to make up for somewhat questionable implementation

--- bancho/clients/client_handle.go
+++ bancho/clients/client_handle.go
@@ -462,52 +462,64 @@ func (client *Client) HandleIncoming() {
 				go func() {
 					infoReply := packets.BeatmapInfoReply{}
 
+					//Initially store the user ids for the prepared statement
 					queryArguments := []interface{}{
 						client.OsuStats.UserID, client.OsuStats.UserID, client.OsuStats.UserID,
 					}
 
+					//will store the prepared statement question marks for the filenames
 					questionMarks := ""
 
+					//edge case for no filenames, it will still have at least 1 filename, even though itll be empty
 					if len(infoRequest.Filenames) == 0 {
 						questionMarks = "?"
 						queryArguments = append(queryArguments, "")
 					} else {
+						//for every filename add a question mark
 						for i := 0; i != len(infoRequest.Filenames); i++ {
 							questionMarks += "?, "
 						}
 					}
 
+					//trim off the last comma to not cause massive issues
 					questionMarks = strings.TrimSuffix(questionMarks, ", ")
 
+					//will store the beatmap ids for the sql
 					beatmapIds := ""
 
+					//edge case for no beatmap ids, it will still have at least 1 beatmap id, even though itll be 0
 					if len(infoRequest.BeatmapIds) == 0 {
 						beatmapIds = "0"
 					} else {
+						//add every beatmap id
 						for i := 0; i != len(infoRequest.BeatmapIds); i++ {
 							beatmapIds += string(infoRequest.BeatmapIds[i]) + ", "
 						}
 					}
 
+					//trim off comma to not have a extra one
 					beatmapIds = strings.TrimSuffix(beatmapIds, ", ")
 
+					//the absolutely gigantic sql
 					sql := fmt.Sprintf("SELECT result.beatmap_id, result.beatmapset_id, result.filename, result.beatmap_md5, result.ranking_status, result.final_osu_ranking AS 'osu_ranking', result.final_taiko_ranking AS 'taiko_ranking', result.final_catch_ranking AS 'catch_ranking' FROM (SELECT beatmaps.beatmap_id, beatmaps.beatmapset_id, beatmaps.filename, beatmaps.beatmap_md5, beatmaps.ranking_status, osuResult.ranking AS 'osu_ranking', osuResult.user_id AS 'osu_user_id', taikoResult.ranking AS 'taiko_ranking', taikoResult.user_id AS 'taiko_user_id', catchResult.ranking AS 'catch_ranking', catchResult.user_id AS 'catch_user_id',CASE WHEN osuResult.ranking IS NULL THEN 'N' ELSE osuResult.ranking END AS 'final_osu_ranking',CASE WHEN taikoResult.ranking IS NULL THEN 'N' ELSE taikoResult.ranking END AS 'final_taiko_ranking', CASE WHEN catchResult.ranking IS NULL THEN 'N' ELSE catchResult.ranking END AS 'final_catch_ranking'FROM waffle.beatmaps LEFT JOIN scores osuResult ON osuResult.beatmap_id = beatmaps.beatmap_id AND osuResult.mapset_best = 1 AND osuResult.playmode = 0 AND osuResult.user_id = ? LEFT JOIN scores taikoResult ON taikoResult.beatmap_id = beatmaps.beatmap_id AND taikoResult.mapset_best = 1 AND taikoResult.playmode = 1 AND taikoResult.user_id = ? LEFT JOIN scores catchResult ON catchResult.beatmap_id = beatmaps.beatmap_id AND catchResult.mapset_best = 1 AND catchResult.playmode = 2 AND catchResult.user_id = ? WHERE beatmaps.filename IN ( %s ) OR beatmaps.beatmap_id IN ( %s )) result", questionMarks, beatmapIds)
 
+					//add the filenames as query arguments
 					for i := 0; i != len(infoRequest.Filenames); i++ {
 						queryArguments = append(queryArguments, infoRequest.Filenames[i])
 					}
 
+					//query
 					databaseQuery, databaseQueryErr := database.Database.Query(sql, queryArguments...)
 
+					//momentarily nonexistant error handling
 					if databaseQueryErr != nil {
 						if databaseQuery != nil {
 							databaseQuery.Close()
 						}
-
-						fmt.Printf("fuck\n")
 					}
 
 					if databaseQuery != nil {
+						//for every database result
 						for databaseQuery.Next() {
 							beatmapInfo := packets.BeatmapInfo{}
 
@@ -518,6 +530,7 @@ func (client *Client) HandleIncoming() {
 
 							databaseQuery.Scan(&beatmapInfo.BeatmapId, &beatmapInfo.BeatmapSetId, &osuFilename, &beatmapInfo.BeatmapChecksum, &beatmapInfo.Ranked, &osuRank, &taikoRank, &catchRank)
 
+							//convert string rank to peppys enum ranking
 							switch osuRank {
 							case "XH":
 								beatmapInfo.OsuRank = 0
@@ -551,6 +564,7 @@ func (client *Client) HandleIncoming() {
 								break
 							}
 
+							//convert string rank to peppys enum ranking
 							switch taikoRank {
 							case "XH":
 								beatmapInfo.TaikoRank = 0
@@ -584,6 +598,7 @@ func (client *Client) HandleIncoming() {
 								break
 							}
 
+							//convert string rank to peppys enum ranking
 							switch catchRank {
 							case "XH":
 								beatmapInfo.CatchRank = 0
@@ -617,8 +632,10 @@ func (client *Client) HandleIncoming() {
 								break
 							}
 
+							//will store the index of the info request the client gave us
 							infoPosition := int16(-1)
 
+							//find it
 							for k := 0; k != len(infoRequest.Filenames); k++ {
 								if infoRequest.Filenames[k] == osuFilename {
 									infoPosition = int16(k)
@@ -627,11 +644,14 @@ func (client *Client) HandleIncoming() {
 
 							beatmapInfo.InfoId = infoPosition
 
+							//append to the reply list
 							infoReply.BeatmapInfos = append(infoReply.BeatmapInfos, beatmapInfo)
 						}
 
+						//send off
 						packets.BanchoSendBeatmapInfoReply(client.PacketQueue, infoReply)
 
+						//make sure to close the connection
 						databaseQuery.Close()
 					}
 				}()
